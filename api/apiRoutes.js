ee9a196fac1c83040d79d22284d5833acfb01b1e
Jason Loomis <jason@jrloomis.com>
2019-11-11 17:00:03 -0500

    import auth-router and userRouter

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -1,5 +1,13 @@
+// const bcrypt = require("bcryptjs");
 const router = require("express").Router();
 
+const authRouter = require("../auth/auth-router");
+const usersRouter = require("../routes/userRouter");
+
+router.use("/auth", authRouter);
+router.use("/users", usersRouter);
+
+// * sanity
 router.get("/", (req, res) => {
   res.json({ api: "Good news everyone" });
 });
